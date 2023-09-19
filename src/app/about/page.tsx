cd51d3badbb1916aa16aa1ad0f559792744c840b
Keerthini R <Keerthini.r@accionlabs.in>
2023-09-19 19:50:37 +0530

    css module

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./about.module.css"
+const about = () => {
+  return (
+    <div>
+            <h1 className={style.header}>Welcome</h1>
+    </div>
+  )
+}
+
+export default about
