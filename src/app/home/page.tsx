6d1d2ea3f74e43613038dcabc1bb0c89895fdb76
Keerthini R <Keerthini.r@accionlabs.in>
2023-09-19 14:44:22 +0530

    sass and module css

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import homestyle from '../home/home.module.scss'
+
+const home = () => {
+  return (
+    <div className={homestyle.header}>
+      home
+    </div>
+  )
+}
+
+export default home
