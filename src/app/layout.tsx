2f1201f8c555e7f546bb0b6ca4b8b0e76e9a18fc
Keerthini R <Keerthini.r@accionlabs.in>
2023-09-20 11:48:18 +0530

    font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+// import { Inter } from 'next/font/google'
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" 
+          rel="stylesheet"
+        /> */}
+      </head>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body>{children}</body>
+
     </html>
   )
 }
